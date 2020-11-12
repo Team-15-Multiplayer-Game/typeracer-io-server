eb3932939f89b588405966ce9865468ec6413b28
erick <erick.gunawan70@gmail.com>
2020-11-13 02:57:52 +0700

    edited socket on app.js

--- server/app.js
+++ server/app.js
@@ -12,8 +12,15 @@ app.use(cors())
 app.use(express.json())
 app.use(urlencoded({extended: true}))
 
+let users = []
+
 io.on('connection', socket => {
   console.log('A user connected')
+  socket.on('userLogin', (username) => {
+    console.log(username, 'ini username server')
+    users.push(username)
+    
+  })
 })
 
 http.listen(3000, () => {
